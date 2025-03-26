d867b8cc7c6a900d760714f296e01867de5a726f
kqatte <kqattte@gmail.com>
2025-03-27 01:03:52 +0300

    adaptivefinal

--- javascripts/javascripts.js
+++ javascripts/javascripts.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   PsevdoDraggable1()
   PsevdoDraggable2()
   PsevdoDraggable3()
+  PsevdoinitCanvas()
 })
 
 // ПЕРВЫЙ ЭКРАН
@@ -282,26 +283,15 @@ function Draggable5() {
 
 function initCanvas() {
   var canvas = document.getElementById('canvas')
-  if (!canvas) {
-    console.error('Canvas element not found!')
-    return
-  }
-
   var ctx = canvas.getContext('2d')
-  if (!ctx) {
-    console.error('2D context is not supported!')
-    return
-  }
   const dpr = window.devicePixelRatio || 1
   canvas.style.width = '100vw'
   canvas.style.height = '65vw'
   canvas.width = Math.floor(window.innerWidth * dpr)
   canvas.height = Math.floor(window.innerWidth * 0.65 * dpr)
   ctx.scale(dpr, dpr)
-
   ctx.strokeStyle = '#740606'
   ctx.lineWidth = 12
-
   function getMousePos(canvas, evt) {
     var rect = canvas.getBoundingClientRect()
     return {
@@ -309,13 +299,11 @@ function initCanvas() {
       y: evt.clientY - rect.top
     }
   }
-
   function mouseMove(evt) {
     var mousePos = getMousePos(canvas, evt)
     ctx.lineTo(mousePos.x, mousePos.y)
     ctx.stroke()
   }
-
   canvas.addEventListener('mousedown', function (evt) {
     var mousePos = getMousePos(canvas, evt)
     ctx.beginPath()
@@ -323,15 +311,12 @@ function initCanvas() {
     evt.preventDefault()
     canvas.addEventListener('mousemove', mouseMove, false)
   })
-
   canvas.addEventListener('mouseup', function () {
     canvas.removeEventListener('mousemove', mouseMove, false)
   })
-
   canvas.addEventListener('mouseout', function () {
     canvas.removeEventListener('mousemove', mouseMove, false)
   })
-
   document.getElementById('clear').addEventListener('click', function () {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
   })
@@ -504,7 +489,7 @@ function PsevdoDraggable2() {
     }
   }
 }
-function PsevdoDraggable1() {
+function PsevdoDraggable3() {
   dragElement(document.getElementById('psevdosilhouette3'))
   function dragElement(elmnt) {
     let pos1 = 0,
@@ -540,3 +525,80 @@ function PsevdoDraggable1() {
     }
   }
 }
+
+// АДАПТИВКА ЧЕТВЕРТЫЙ ЭКРАН
+
+function PsevdoinitCanvas() {
+  var canvas = document.getElementById('psevdocanvas')
+  var ctx = canvas.getContext('2d')
+  const dpr = window.devicePixelRatio || 1
+  canvas.style.width = '100vw'
+  canvas.style.height = '130vw'
+  canvas.width = Math.floor(window.innerWidth * dpr)
+  canvas.height = Math.floor(window.innerWidth * 1.3 * dpr)
+  ctx.scale(dpr, dpr)
+  ctx.strokeStyle = '#740606'
+  ctx.lineWidth = 10
+  function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect()
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    }
+  }
+  function mouseMove(evt) {
+    var mousePos = getMousePos(canvas, evt)
+    ctx.lineTo(mousePos.x, mousePos.y)
+    ctx.stroke()
+  }
+  canvas.addEventListener('mousedown', function (evt) {
+    var mousePos = getMousePos(canvas, evt)
+    ctx.beginPath()
+    ctx.moveTo(mousePos.x, mousePos.y)
+    evt.preventDefault()
+    canvas.addEventListener('mousemove', mouseMove, false)
+  })
+  canvas.addEventListener('mouseup', function () {
+    canvas.removeEventListener('mousemove', mouseMove, false)
+  })
+  canvas.addEventListener('mouseout', function () {
+    canvas.removeEventListener('mousemove', mouseMove, false)
+  })
+  document.getElementById('psevdoclear').addEventListener('click', function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+  })
+}
+// ПОДВАЛ АДАПТИВКА
+document.addEventListener('DOMContentLoaded', () => {
+  const mainObject = document.getElementById('psevdohintbase1')
+  const secondaryObject = document.getElementById('psevdoanna')
+
+  mainObject.addEventListener('click', () => {
+    mainObject.style.opacity = '0'
+    mainObject.style.pointerEvents = 'none'
+
+    secondaryObject.style.opacity = '1'
+  })
+})
+document.addEventListener('DOMContentLoaded', () => {
+  const mainObject = document.getElementById('psevdohintbase2')
+  const secondaryObject = document.getElementById('psevdokate')
+
+  mainObject.addEventListener('click', () => {
+    mainObject.style.opacity = '0'
+    mainObject.style.pointerEvents = 'none'
+
+    secondaryObject.style.opacity = '1'
+  })
+})
+document.addEventListener('DOMContentLoaded', () => {
+  const mainObject = document.getElementById('psevdohintbase3')
+  const secondaryObject = document.getElementById('psevdokamila')
+
+  mainObject.addEventListener('click', () => {
+    mainObject.style.opacity = '0'
+    mainObject.style.pointerEvents = 'none'
+
+    secondaryObject.style.opacity = '1'
+  })
+})
