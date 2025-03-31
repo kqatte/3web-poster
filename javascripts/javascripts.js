8d65084533fb26b9ea337ddb7913dbfe207b97e2
kqatte <kqattte@gmail.com>
2025-03-31 15:48:38 +0300

    preloader

--- javascripts/javascripts.js
+++ javascripts/javascripts.js
@@ -18,8 +18,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const preloader = document.getElementById('preloader')
   const content = document.getElementById('content')
   window.addEventListener('load', () => {
-    preloader.style.display = 'none'
-    content.style.display = 'block'
+    setTimeout(() => {
+      preloader.style.display = 'none'
+      content.style.display = 'block'
+    }, 3000)
   })
 })
 
